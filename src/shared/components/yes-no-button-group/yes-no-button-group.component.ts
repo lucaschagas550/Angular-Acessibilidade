94ef1003c56600de645d2d7767f4ca0d1b7a1b7d
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-12-22 19:20:36 -0300

    feat: interface control value acessor

--- src/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,17 +1,37 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss']
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   @Input() public value: string = null;
   @Input() public label: string = '';
   @Output() public valueChange = new EventEmitter<string>(); // emite um evento de mudanca com binding
   public options = YesNoButtonGroupOptions;
+  public onChange = (value: string) => { };
+  public onTouched = () => { };
 
   constructor() { }
+
+  public writeValue(value: string): void {
+    this.value = value;
+    this.onChange(value);
+  }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+  
+  public setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
