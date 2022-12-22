3a93d4c166347991e967de2a49e1adcf5cd5d99e
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-12-21 22:07:40 -0300

    feat: event binding

--- src/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class YesNoButtonGroupComponent implements OnInit {
   @Input() public value: string = null;
   @Input() public label: string = '';
+  @Output() public valueChange = new EventEmitter<string>(); // emite um evento de mudanca com binding
   public options = YesNoButtonGroupOptions;
 
   constructor() { }
@@ -17,6 +18,7 @@ export class YesNoButtonGroupComponent implements OnInit {
 
   public activate(value: string): void {
     this.value = value;
+    this.valueChange.emit(this.value); //notifica mundo externo que mudou valor
   }
 }
 
