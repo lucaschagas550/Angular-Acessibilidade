506f82ab8c8143316a77823f47f68bd230cca7b5
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-12-22 19:42:34 -0300

    feat: ng value accessor

--- src/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,10 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
-  styleUrls: ['./yes-no-button-group.component.scss']
+  styleUrls: ['./yes-no-button-group.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => YesNoButtonGroupComponent)
+    }
+  ]
 })
 export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   @Input() public value: string = null;
@@ -15,6 +22,9 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   public onTouched = () => { };
 
   constructor() { }
+
+  ngOnInit(): void {
+  }
 
   public writeValue(value: string): void {
     this.value = value;
@@ -28,17 +38,13 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   public registerOnTouched(fn: () => void): void {
     this.onTouched = fn;
   }
-  
+
   public setDisabledState?(isDisabled: boolean): void {
     throw new Error('Method not implemented.');
   }
 
-  ngOnInit(): void {
-  }
-
   public activate(value: string): void {
-    this.value = value;
-    this.valueChange.emit(this.value); //notifica mundo externo que mudou valor
+    this.writeValue(value);
   }
 }
 
